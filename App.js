fc2cca2fa4adbca02c8b5261709af2e185a3d757
Alkis <gkreen@oktopay.eu>
2023-10-09 15:19:24 +0300

    feature-1 is ready

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+//Components
+import Greetings from "./src/components/Greetings";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>CICD example course</Text>
       <StatusBar style="auto" />
+      <Greetings />
     </View>
   );
 }
